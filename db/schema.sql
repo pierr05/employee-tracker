e789957164f787fcfc0b88926d02efe0e5ec997d
pierr05 <pierreachlet322@gmail.com>
2023-04-26 20:24:56 -0400

    added schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employee_db; 
+CREATE DATABASE employee_db; 
+
+USE employee_db; 
+
+CREATE TABLE department (
+    department_id INT NOT NULL auto_increment PRIMARY KEY, 
+    department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL auto_increment PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(),
+    department_id INT FOREIGN KEY REFERENCES department (department_id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL auto_increment PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT
+);
+
