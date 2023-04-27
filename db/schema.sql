afe7075db75ecd15c7ffbcabaa22124089e64f93
pierr05 <pierreachlet322@gmail.com>
2023-04-27 10:53:46 -0400

    added code schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,23 +4,24 @@ CREATE DATABASE employee_db;
 USE employee_db; 
 
 CREATE TABLE department (
-    department_id INT NOT NULL auto_increment PRIMARY KEY, 
+    department_id INT NOT NULL PRIMARY KEY, 
     department_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
-    id INT NOT NULL auto_increment PRIMARY KEY,
+CREATE TABLE role_table (
+    role_table_id INT NOT NULL auto_increment PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(),
+    salary DECIMAL(18, 2),
     -- to hold reference to department role belongs to
-    department_id INT FOREIGN KEY REFERENCES department (department_id)
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department (department_id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
     id INT NOT NULL auto_increment PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT
+    role_table_id INT NOT NULL,
+     FOREIGN KEY (role_table_id)  REFERENCES role_table (role_table_id) ON DELETE CASCADE
 );
 
